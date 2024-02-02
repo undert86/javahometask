ae8e831507d409508908fe250bb2aefcd4680a80
Aleksey Smirnov <aleksey.smirnov@net.ik>
2024-02-02 12:22:16 +0300

    privet

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int floor = 10;
+        int kv = 5;
+        int b = 0;
+        for (int i = 1; floor >= i; i++)
+        {
+            for (int j = 1; kv >= j; j++)
+            {
+                b++;
+                System.out.println("Этаж " + i + ", " + b + ", доставлено");
+
+                java.lang.String a = "dtrfyghjnmknhuyrdftyhijouiygt56r234567890";
+            }
+        }
+    }
+}
